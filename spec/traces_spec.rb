b013d6338a0b55c568968881b3a350f047265d3c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-10-15 19:20:37 +1300

    Fix specs.

--- spec/traces_spec.rb
+++ spec/traces_spec.rb
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require 'traces/provider'
+require 'traces'
 
 class MyClass
 	def my_method(argument)
