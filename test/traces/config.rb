f17fa1e9fcf753d00b2ecedca40a5e9bab02a145
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-01-31 13:59:33 +1300

    Fix logging test.

--- test/traces/config.rb
+++ test/traces/config.rb
@@ -6,22 +6,16 @@
 require "traces/config"
 require "json"
 
-require "sus/fixtures/console"
-
 describe Traces::Config do
 	let(:config) {subject.default}
 	
 	with ".require_backend" do
-		include_context Sus::Fixtures::Console::CapturedLogger
-		
 		it "logs a warning if backend cannot be loaded" do
+			expect(config).to receive(:warn).and_return(nil)
+			
 			expect(
 				config.require_backend({"TRACES_BACKEND" => "traces/backend/missing"})
 			).to be == false
-			
-			expect_console.to have_logged(
-				severity: be == :warn,
-			)
 		end
 	end
 end
